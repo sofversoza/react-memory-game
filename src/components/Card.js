87246fce87b7b5a849b6af97425c5f070f6d6825
sofiaversoza <sofversoza@gmail.com>
2022-08-16 14:21:59 -0700

    FIN

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,20 @@
 import './Card.css'
 
 function Card({ card, handleChoice, flipped, disabled }) {
-	
   const handleClick = () => {
     if (!disabled) {
       handleChoice(card)
     }
   }
   
-  
   return (
 		<div className='card'>
 			<div className={flipped ? 'flipped' : ''}>
-				<img className='front' src={card.src} alt='card front' />
+				<img 
+          className='front' 
+          src={card.src} 
+          alt='card front' 
+        />
 				<img 
           className='back' 
           src='/img/cover-1.jpeg' 
